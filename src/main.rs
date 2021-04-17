60283af63403c3e807bed68e59f62493b7ca30d7
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-04-17 22:52:15 +1000

    Show ‘x’ at snake location

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 mod snake;
 
+use crossterm::{cursor, queue, terminal};
 use std::io;
 use structopt::StructOpt;
 
 fn main() -> anyhow::Result<()> {
     let opts = Opts::from_args();
-    let stdout = io::stdout();
+    let mut stdout = io::stdout();
+
+    terminal::enable_raw_mode()?;
+    queue!(stdout, cursor::Hide)?;
+
     match opts {
-        Opts::Snake => snake::run(stdout),
+        Opts::Snake => snake::run(&mut stdout)?,
     }
+
+    queue!(stdout, cursor::Show)?;
+    terminal::disable_raw_mode()?;
+
+    Ok(())
 }
 
 #[derive(StructOpt)]
--- src/snake.rs
+++ src/snake.rs
@@ -1,11 +1,12 @@
-use crossterm::{event, queue, terminal};
-use std::io;
+use crossterm::{cursor, event, queue, terminal};
+use std::io::{self, Write};
 use std::time::Duration;
 
 const FRAME_RATE: f32 = 60.0;
 
-pub(crate) fn run(mut stdout: io::Stdout) -> anyhow::Result<()> {
+pub(crate) fn run(stdout: &mut io::Stdout) -> anyhow::Result<()> {
     let frame_delta = Duration::from_secs_f32(1.0 / FRAME_RATE);
+    let mut snake_position = (0, 0);
 
     loop {
         queue!(stdout, terminal::Clear(terminal::ClearType::All))?;
@@ -24,14 +25,19 @@ pub(crate) fn run(mut stdout: io::Stdout) -> anyhow::Result<()> {
                 } => break,
 
                 event::KeyEvent { code, .. } => match code {
-                    event::KeyCode::Up | event::KeyCode::Char('w') => {}
-                    event::KeyCode::Down | event::KeyCode::Char('s') => {}
-                    event::KeyCode::Left | event::KeyCode::Char('a') => {}
-                    event::KeyCode::Right | event::KeyCode::Char('d') => {}
+                    event::KeyCode::Up | event::KeyCode::Char('w') => snake_position.1 -= 1,
+                    event::KeyCode::Down | event::KeyCode::Char('s') => snake_position.1 += 1,
+                    event::KeyCode::Left | event::KeyCode::Char('a') => snake_position.0 -= 1,
+                    event::KeyCode::Right | event::KeyCode::Char('d') => snake_position.0 += 1,
                     _ => {}
                 },
             }
-        };
+        }
+
+        queue!(stdout, cursor::MoveTo(snake_position.0, snake_position.1))?;
+        write!(stdout, "x")?;
+
+        stdout.flush()?;
     }
 
     Ok(())
